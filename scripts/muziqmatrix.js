0154efc892555d6b550d19392b65201fef3b9fe9
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-05-29 18:30:33 +0100

    Test and create enter button function to hide landing page and canvas

--- scripts/muziqmatrix.js
+++ scripts/muziqmatrix.js
@@ -1,5 +1,17 @@
 
+// Function to hide landing page when enter button is clicked
+const enterBtn = () => {
+  ["landing-page", "canvas"].forEach(id => 
+    document.getElementById(id).style.display = "none"
+  );
+};
 
+// Enter button function
+const enterBtnClick = () =>
+document.getElementById("enter-btn")?.addEventListener("click", enterBtn);
+
+// Call function for event listener
+enterBtnClick();
 
 //Export functions to be used in test
-module.exports = {  }
+module.exports = { enterBtn, enterBtnClick };
--- scripts/tests/muziqmatrix.test.js
+++ scripts/tests/muziqmatrix.test.js
@@ -3,19 +3,23 @@
  */
 
 //Import functions to test
-const {  } = require("../muziqmatrix");
+const { enterBtnClick  } = require("../muziqmatrix");
 
 //Runs before each test is run
 // Creates a mock html to test on
 beforeEach(() => {
-    document.body.innerHTML = ``;
-});
-
-
-//Setup test environment + load HTML
-beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
     document.body.innerHTML = fileContents;
-})
+    enterBtnClick();
+});
 
+// Tests for landing page
+describe("Landing page tests", () => {
+    test("removes landing page click enter button", () => {
+        const btn = document.getElementById("enter-btn");
+            btn.click();
+            expect(document.getElementById("landing-page").style.display).toBe("none");
+            expect(document.getElementById("canvas").style.display).toBe("none");
+    });
+});
