68af7b5536a3c6350e9a65ae3e30c9e17440c1ab
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-05-29 19:47:49 +0100

    Test and Show music quiz when game button is clicked

--- scripts/muziqmatrix.js
+++ scripts/muziqmatrix.js
@@ -1,4 +1,7 @@
 
+//Import for jquery shorthand
+global.$ = require("jquery");
+
 // Function to hide landing page when enter button is clicked
 const enterBtn = () => {
   ["landing-page", "canvas"].forEach(id => 
@@ -9,9 +12,14 @@ const enterBtn = () => {
 // Enter button function
 const enterBtnClick = () =>
 document.getElementById("enter-btn")?.addEventListener("click", enterBtn);
-
-// Call function for event listener
+  // Call function for event listener
 enterBtnClick();
 
+//Show quiz container when games button is clicked
+const gamesBtn = () => {
+  $(".games-btn").on("click", () => $("#quiz-container").removeClass("hidden"));
+};
+
+
 //Export functions to be used in test
-module.exports = { enterBtn, enterBtnClick };
+module.exports = { enterBtn, enterBtnClick, gamesBtn };
--- scripts/tests/muziqmatrix.test.js
+++ scripts/tests/muziqmatrix.test.js
@@ -3,7 +3,7 @@
  */
 
 //Import functions to test
-const { enterBtnClick  } = require("../muziqmatrix");
+const { enterBtnClick, gamesBtn  } = require("../muziqmatrix");
 
 //Runs before each test is run
 // Creates a mock html to test on
@@ -12,6 +12,7 @@ beforeEach(() => {
     let fileContents = fs.readFileSync("index.html", "utf-8");
     document.body.innerHTML = fileContents;
     enterBtnClick();
+    gamesBtn();
 });
 
 // Tests for landing page
@@ -22,4 +23,11 @@ describe("Landing page tests", () => {
             expect(document.getElementById("landing-page").style.display).toBe("none");
             expect(document.getElementById("canvas").style.display).toBe("none");
     });
+});
+
+describe("Music quiz tests", () => {
+    test("quiz container shows when games button is clicked", () => {
+    $(".games-btn").trigger("click");
+    expect($("#quiz-container").hasClass("hidden")).toBe(false);
+  });
 });
